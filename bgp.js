fdc6c2fcc62920ecece2bd3ca8f766902c1f66e1
maxtar <costin.botescu@gmail.com>
2021-03-06 17:28:16 +0200

    grab xhr

--- bgp.js
+++ bgp.js
@@ -1,13 +1,39 @@
 var urls;
+function verif(details){
+	var d=details.responseHeaders;
+	for (a in d){
+		var e=d[a]; 
+		if(e.name=="Content-Range"){
+			var b='bytes 0';
+			return e.value.substr(0,b.length)==b
+		}
+	}
+	var b=new URL(details.url);
+	var c=new URLSearchParams(b.search);
+	if(c.has('range'))
+		return c.get('range')[0]=='0';
+	return false
+}
+function starter(details,media){
+	if(verif(details))
+		urls=[details.url,media]
+}
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
 	var d=details.responseHeaders;
-	//xhr
-	if (details.type=='media'){
+	var media=details.type=='media';
+	if (media){
+		starter(details,media)
+	}else if(details.type=='xmlhttprequest'){
 		for (a in d){
-			var e=d[a]; 
-			if(e.name=="Content-Range"){
-				if(e.value.substr(0,7)=='bytes 0'){
-					urls=details.url
+			var e=d[a];
+			if(e.name=="Content-Type"){
+				var b=['video/mp4','audio/mp4'];
+				for (x in b){
+					var f=b[x];
+					if(e.value.substr(0,f.length)==f){
+						starter(details,media);
+						break
+					}
 				}
 				break
 			}
--- popup.js
+++ popup.js
@@ -2,10 +2,26 @@ chrome.runtime.sendMessage(null,//{greeting: "urls"},
 function (response) {
 	if(response){
 		var btn = document.createElement("BUTTON");
-		btn.innerHTML = response;
-		btn.addEventListener ("click", function() {
-			chrome.downloads.download({url: this.innerHTML},function(id) { })
-		});
+		btn.innerHTML = response[0];
+		if(response[1]){
+			btn.addEventListener ("click", function() {
+				var a=unescap(this.innerHTML);
+				chrome.downloads.download({url: a},function(id) { })
+			})
+		}else{
+			btn.addEventListener ("click", function() {
+				var a=unescap(this.innerHTML);
+				var b=new URL(a);
+				var c=new URLSearchParams(b.search);
+				c.set('range','0-2147483647');
+				a=b.origin+b.pathname+'?'+c.toString();
+				window.open(a,'_blank')
+			})
+		}
 		document.body.appendChild(btn)
 	}
-})
+});
+function unescap(a){
+	var doc = new DOMParser().parseFromString(a, "text/html");
+	return doc.documentElement.textContent
+}
