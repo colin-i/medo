1aa3723c39cc4d1fe2d2462388fb2b6c720172bc
maxtar <costin.botescu@gmail.com>
2021-03-17 17:17:34 +0200

    save range

--- options.js
+++ options.js
@@ -4,15 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
 		var content = document.createElement("DIV");
 		var t=document.createTextNode("Request range");
 		var input = document. createElement("input");
+		input.value=options.range;
 		content.appendChild(t);
 		content.appendChild(input);
 		var btn = document.createElement("BUTTON");
-		btn.innerHTML = "Hide";
+		btn.innerHTML = "Done";
 		btn.addEventListener ("click", function() {
-			content.remove()
+			options.range=input.value;
+			content.remove();
+			chrome.storage.sync.set({ range: options.range })
 		});
 		content.appendChild(btn);
 		this.parentNode.insertBefore(content, this.nextSibling)//if last,null is ok
+	});
+	chrome.storage.sync.get('range', function(data) {
+		if(data.range)options.range=data.range
 	})
 })
 var options = (function (){return{
